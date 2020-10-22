3de95fcc1377f38314606c17c0b731147c5bf18e
mlavi16 <Maya.lavi11@gmail.com>
2020-10-22 08:17:21 -0400

    adding imageio.h

--- imageio.h
+++ imageio.h
@@ -3,7 +3,5 @@
 const int MAX_H = 512;
 const int MAX_W = 512;
 
-void readImage(std::string filename, int image[MAX_H][MAX_W], int &height, int &
-width);
-void writeImage(std::string filename, int image[MAX_H][MAX_W], int height, int w
-idth);
+void readImage(std::string filename, int image[MAX_H][MAX_W], int &height, int &width);
+void writeImage(std::string filename, int image[MAX_H][MAX_W], int height, int width);
