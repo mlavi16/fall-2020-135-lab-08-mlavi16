1ba3376b78e9d15c4790651988d6df9af93986fe
mlavi16 <Maya.lavi11@gmail.com>
2020-10-22 20:25:11 -0400

    adding writeImage2

--- imageio.h
+++ imageio.h
@@ -2,6 +2,10 @@
 
 const int MAX_H = 512;
 const int MAX_W = 512;
+const int MAX_H2 = 1024;
+const int MAX_W2 = 1024;
+
 
 void readImage(std::string filename, int image[MAX_H][MAX_W], int &height, int &width);
 void writeImage(std::string filename, int image[MAX_H][MAX_W], int height, int width);
+void writeImage2(std::string filename, int image[MAX_H2][MAX_W2], int height, int width);
